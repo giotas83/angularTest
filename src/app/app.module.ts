09298d5ff609ef3e87c0c30ad77fa70e305a1e5e
Giovanni Tasca <giovanni.tasca@eng.it>
2022-02-14 21:09:54 +0100

    form exercise: create reactive form base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -87,7 +87,8 @@ import { TestInputComponent } from './esercizi-base/forms-15/template-driven-exa
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/esercizi-base/forms-15/reactive-form-example/reactive-form-example.component.ts
+++ src/app/esercizi-base/forms-15/reactive-form-example/reactive-form-example.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form-example',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveFormExampleComponent implements OnInit {
 
+  public form: FormGroup;
+
+  public genders: string[] = ['male', 'female'];
+
   constructor() { }
 
   ngOnInit() {
+    this.createForm();
+  }
+
+  public createForm(): void {
+    this.form = new FormGroup({
+      'username': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'gender': new FormControl('male')
+    })
+  }
+
+  public onSubmit() {
+    console.log(this.form);
   }
 
 }
