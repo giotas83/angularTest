41a255d0c245139be4028ab41dfb4faa11ad417f
Giovanni Tasca <giovanni.tasca@eng.it>
2022-02-15 10:44:01 +0100

    exercise form react: add custom validator

--- src/app/esercizi-base/forms-15/reactive-form-example/reactive-form-example.component.ts
+++ src/app/esercizi-base/forms-15/reactive-form-example/reactive-form-example.component.ts
@@ -12,6 +12,9 @@ export class ReactiveFormExampleComponent implements OnInit {
 
   public genders: string[] = ['male', 'female'];
 
+  // per validatore nomi vietati
+  private forbiddenNames: string[] = ['giovanni', 'paola'];
+
   get hobbiesControls() {
     return (this.form.get('hobbies') as FormArray).controls;
   }
@@ -25,7 +28,7 @@ export class ReactiveFormExampleComponent implements OnInit {
   public createForm(): void {
     this.form = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNamesValidator.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
       'gender': new FormControl('male'),
@@ -36,6 +39,14 @@ export class ReactiveFormExampleComponent implements OnInit {
   public onAddHobby(): void {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.form.get('hobbies')).push(control);
+  }
+
+  // validatore custom - scateno un errore se inserisco un nome vietato
+  private forbiddenNamesValidator(control: FormControl): {[s: string]: boolean} {
+    if(this.forbiddenNames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null; // non si passa oggetto con false !
   }
 
   public onSubmit() {
